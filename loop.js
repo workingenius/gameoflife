491887c9c0cd2bd01b8e70de7c5ac2db6ecb8b2f
Ruqi Shang <workingenius@163.com>
2017-02-25 18:56:58 +0800

    Implement pause

---
+++ loop.js
@@ -0,0 +1,22 @@
+(function() {
+  glob.loop = function(stepProc) {
+    var timer;
+    return {
+      interval: 2000,
+      start: function() {
+        timer = setInterval(stepProc, this.interval);
+      },
+      pause: function() {
+        timer && clearInterval(timer);
+        timer = null;
+      },
+      isOn: function() {
+        return !!timer;
+      },
+      shift: function() {
+        if (this.isOn()) this.pause();
+        else this.start();
+      },
+    };
+  };
+})();
