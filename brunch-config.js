62e45715c82325e8c6e6b8c6dd95be3d049f8ede
Ruqi Shang <workingenius@163.com>
2017-03-09 21:54:07 +0800

    Using brunch to manage building flow

---
+++ brunch-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  files: {
+    javascripts: {
+      joinTo: {
+        'js/app.js': /^app/,
+      }
+    },
+    stylesheets: {
+      joinTo: {
+        'css/app.css': /^app/,
+      }
+    }
+  }
+};
