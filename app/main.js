82e291cf2cfd95326ce96b29fb9e5ac477c115fb
Ruqi Shang <workingenius@163.com>
2017-03-09 22:19:02 +0800

    Commonjs main.js and mouse.js

--- app/main.js
+++ app/main.js
@@ -1,74 +1,71 @@
-(function() {
-  var lifeM = require('life');
-  var indexM = require('index');
-  var groundM = require('ground');
-  var viewM = require('view');
-  var loop = require('loop');
+var lifeM = require('life');
+var indexM = require('index');
+var groundM = require('ground');
+var viewM = require('view');
+var loop = require('loop');
 
-  (function initArray() {
-    var array;
+(function initArray() {
+  var array;
 
-    glob.array = lifeM.createBeings(indexM.createIndices(groundM.rowCount, groundM.colCount));
-    array = glob.array;
+  glob.array = lifeM.createBeings(indexM.createIndices(groundM.rowCount, groundM.colCount));
+  array = glob.array;
 
-    // initArray
-    plane(array, [3, 3]);
-    plane(array, [7, 3]);
-    plane(array, [3, 7]);
-    plane(array, [7, 7]);
+  // initArray
+  plane(array, [3, 3]);
+  plane(array, [7, 3]);
+  plane(array, [3, 7]);
+  plane(array, [7, 7]);
 
-    viewM.drawArray(array);
-  })();
-
-  function stepProc() {
-    var changings = lifeM.calcChangings();
-    lifeM.propagate(changings);
-    viewM.drawChangings(glob.array, changings);
-  }
-
-  glob.onOff = loop(stepProc);
-
-
-  // following codes should move elsewhere
-  
-  function plane(array, center) {
-    lifeM.setState(array, up(center), lifeM.ALIVED);
-    lifeM.setState(array, right(center), lifeM.ALIVED);
-    lifeM.setState(array, downright(center), lifeM.ALIVED);
-    lifeM.setState(array, down(center), lifeM.ALIVED);
-    lifeM.setState(array, downleft(center), lifeM.ALIVED);
-  }
-
-  function up(point) {
-    return [point[0], point[1] - 1];
-  }
-
-  function down(point) {
-    return [point[0], point[1] + 1];
-  }
-
-  function left(point) {
-    return [point[0] - 1, point[1]];
-  }
-
-  function right(point) {
-    return [point[0] + 1, point[1]];
-  }
-
-  function upleft(p) {
-    return up(left(p));
-  }
-
-  function upright(p) {
-    return up(right(p));
-  }
-
-  function downleft(p) {
-    return down(left(p));
-  }
-
-  function downright(p) {
-    return down(right(p));
-  }
-
+  viewM.drawArray(array);
 })();
+
+function stepProc() {
+  var changings = lifeM.calcChangings();
+  lifeM.propagate(changings);
+  viewM.drawChangings(glob.array, changings);
+}
+
+glob.onOff = loop(stepProc);
+
+
+// following codes should move elsewhere
+
+function plane(array, center) {
+  lifeM.setState(array, up(center), lifeM.ALIVED);
+  lifeM.setState(array, right(center), lifeM.ALIVED);
+  lifeM.setState(array, downright(center), lifeM.ALIVED);
+  lifeM.setState(array, down(center), lifeM.ALIVED);
+  lifeM.setState(array, downleft(center), lifeM.ALIVED);
+}
+
+function up(point) {
+  return [point[0], point[1] - 1];
+}
+
+function down(point) {
+  return [point[0], point[1] + 1];
+}
+
+function left(point) {
+  return [point[0] - 1, point[1]];
+}
+
+function right(point) {
+  return [point[0] + 1, point[1]];
+}
+
+function upleft(p) {
+  return up(left(p));
+}
+
+function upright(p) {
+  return up(right(p));
+}
+
+function downleft(p) {
+  return down(left(p));
+}
+
+function downright(p) {
+  return down(right(p));
+}
--- app/mouse.js
+++ app/mouse.js
@@ -1,41 +1,42 @@
-(function() {
-  var lastIndex = null;
-  var indexModule = require('index');
-  var groundModule = require('ground');
-  var lifeModule = require('life');
-  var viewModule = require('view');
+var indexModule = require('index');
+var groundModule = require('ground');
+var lifeModule = require('life');
+var viewModule = require('view');
 
-  function getMousePoint(mouseEvent) {
-    var {layerX: x, layerY: y} = mouseEvent;
-    return [x, y];
-  }
 
-  function getMouseIndex(mouseEvent) {
-    return groundModule.positionToIndex(...getMousePoint(mouseEvent));
-  }
+var lastIndex = null;
 
-  document.addEventListener('DOMContentLoaded', function(event) {
-    viewModule.canvas.addEventListener('mousemove', function(evt) {
-      var index = getMouseIndex(evt);
-      if (indexModule.indexEq(index, lastIndex)) return;
-      var changings = [{
-        action: 'focus',
-        index: index,
-      }];
-      if (lastIndex) {
-        changings.unshift({
-          action: 'unfocus',
-          index: lastIndex,
-        });
-      }
-      viewModule.drawChangings(glob.array, changings);
-      lastIndex = index;
-    }, false);
 
-    viewModule.canvas.addEventListener('click', function(evt) {
-      var index = getMouseIndex(evt);
-      lifeModule.flip(glob.array, index);
-      viewModule.syncGrid(index);
-    });
+function getMousePoint(mouseEvent) {
+  var {layerX: x, layerY: y} = mouseEvent;
+  return [x, y];
+}
+
+function getMouseIndex(mouseEvent) {
+  return groundModule.positionToIndex(...getMousePoint(mouseEvent));
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  viewModule.canvas.addEventListener('mousemove', function(evt) {
+    var index = getMouseIndex(evt);
+    if (indexModule.indexEq(index, lastIndex)) return;
+    var changings = [{
+      action: 'focus',
+      index: index,
+    }];
+    if (lastIndex) {
+      changings.unshift({
+        action: 'unfocus',
+        index: lastIndex,
+      });
+    }
+    viewModule.drawChangings(glob.array, changings);
+    lastIndex = index;
+  }, false);
+
+  viewModule.canvas.addEventListener('click', function(evt) {
+    var index = getMouseIndex(evt);
+    lifeModule.flip(glob.array, index);
+    viewModule.syncGrid(index);
   });
-})();
+});
